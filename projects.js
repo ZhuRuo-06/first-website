c228d33147642d0ee69a52c8209ae4f58760077c
ZhuRuo-06 <dony.083147726274@gmail.com>
2026-01-09 09:29:27 +0700

    Add files via upload

---
+++ projects.js
@@ -0,0 +1,29 @@
+fetch("projects.json")
+    .then(res => res.json())
+    .then(projects => {
+        const container = document.getElementById("projects");
+        const kosong = document.getElementById("kosong");
+        if (!container) return;
+        if (kosong) kosong.style.display = "none";
+
+
+        projects.forEach(p => {
+            const card = document.createElement("div");
+            card.className = "project-card";
+
+            card.innerHTML = `
+                <img src="${p.thumbnail}" alt="${p.title}">
+                <div class="content">
+                    <h3>${p.title}</h3>
+                    <p>${p.description}</p>
+                </div>
+            `;
+
+            card.onclick = () => {
+                if (p.url) window.location.href = p.url;
+            };
+
+          container.appendChild(card);
+        });
+    })
+    .catch(err => console.error("Gagal load projects:", err));
